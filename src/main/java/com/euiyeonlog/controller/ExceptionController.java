9072e2c3e4ce82e93b57d9ccca6fe1f347d0e50a
kiteof_park <kiteof_park@naver.com>
2025-01-12 19:31:17 +0900

    fix: ErrorResposne 클래스 추가, Exception Controller 수정

--- src/main/java/com/euiyeonlog/controller/ExceptionController.java
+++ src/main/java/com/euiyeonlog/controller/ExceptionController.java
@@ -1,32 +1,42 @@
 package com.euiyeonlog.controller;
 
+import com.euiyeonlog.response.ErrorResposne;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e){
-        // log.error("exceptionHandler error", e);
-        // 받은 Exception을 토대로 에러 처리를해서 JSON형태로 응답을 만들어주는 작업이 필요
-        FieldError fieldError = e.getFieldError();
-        String field = fieldError.getField();
-        String message = fieldError.getDefaultMessage();
+    @ResponseBody   // ControllerAdvice에 ReponseBody가 없으면 메서드 반환값인 Map<String, String>이 View이름으로 해석
+    public ErrorResposne invalidRequestHandler(MethodArgumentNotValidException e){
+        // ErrorResposne response =  new ErrorResposne("400", "잘못된 요청입니다.");
+        ErrorResposne response = ErrorResposne.builder()
+                .code("400")
+                .message("잘못된 요청입니다.")
+                .build();
 
-        // 어떻게 JSON으로 넘겨줄까?
-        Map<String, String> response = new HashMap<>();
-        response.put(field, message);
+        for(FieldError fieldError : e.getFieldErrors()){
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
         return response;
+
+        // ✅ Excepotion에 대한 응답을 클래스로 정의해서 사용
+//        // log.error("exceptionHandler error", e);
+//        // 받은 Exception을 토대로 에러 처리를해서 JSON형태로 응답을 만들어주는 작업이 필요
+//        FieldError fieldError = e.getFieldError();
+//        String field = fieldError.getField();
+//        String message = fieldError.getDefaultMessage();
+//
+//        // 어떻게 JSON으로 넘겨줄까?
+//        Map<String, String> response = new HashMap<>();
+//        response.put(field, message);
+//        return response;
     }
 }
---
+++ src/main/java/com/euiyeonlog/response/ErrorResposne.java
@@ -0,0 +1,42 @@
+package com.euiyeonlog.response;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Exception이 발생했을 때 반환하는 내용을 ErrorResponse 클래스로 정의
+
+/* 📌이런 응답의 문제점은 사용자에게 어떤 값이 잘못됐는지 응답❌
+* {
+*       "code" : "400",
+*       "message" : "잘못된 요청입니다",
+* }
+* */
+
+/* 📌이런 응답의 문제점은 사용자에게 어떤 값이 잘못됐는지 응답❌
+ * {
+ *       "code" : "400",
+ *       "message" : "잘못된 요청입니다",
+ *       "validation" : {
+ *              "title" : "값을 입력해주세요"
+ *       }
+ * }
+ * */
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class ErrorResposne {
+    private final String code;
+    private final String message;
+    private final Map<String, String> validation = new HashMap<>();
+
+    public void addValidation(String fieldName, String errorMessage){
+        this.validation.put(fieldName, errorMessage);
+    }
+}
